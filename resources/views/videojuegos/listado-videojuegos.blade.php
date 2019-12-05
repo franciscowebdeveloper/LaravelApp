555c4d0d847b4759a11c73638b65f968d60c7c55
Francisco <francisco.desarrolloweb@gmail.com>
2019-12-05 02:45:43 +0100

    condicionales blade

--- resources/views/videojuegos/listado-videojuegos.blade.php
+++ resources/views/videojuegos/listado-videojuegos.blade.php
@@ -1,5 +1,20 @@
-<?php
-echo "<h1>$titulo</h1>";
+{{-- interpolacion de llaves para mostrar el contenido de las variables --}}
+<h1>{{$titulo}}</h1>
+<h1>{{$listado_videojuegos[2]}}</h1>
 
-// array
-echo "<h1>$listado_videojuegos[2]</h1>";
+{{-- condicion ternaria (mostrar si existe) --}}
+{{-- php --}}
+<?= isset($creador) ? $creador : 'No hay creador'; ?>
+{{-- blade --}}
+{{ $creador or 'No hay creador'}}
+
+<!-- Condicionales blade -->
+<!-- si titulo da true -->
+@if( $titulo && count($listado_videojuegos) >= 5 )
+    <h1>El titulo existe y es este {{$titulo}}</h1>
+@elseif($titulo)
+    <h1>El titulo existe y el listado no es mayor a 5</h1>
+@else
+    <h1>El titulo no existe</h1>
+@endif
+
