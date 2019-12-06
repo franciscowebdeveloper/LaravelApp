e91b258bd843166bdaa8f2185f7b89ff4a8d2740
Francisco <francisco.desarrolloweb@gmail.com>
2019-12-07 00:46:15 +0100

    Migraciones con SQL

--- database/migrations/2019_12_06_203935_create_usuarios_table.php
+++ database/migrations/2019_12_06_203935_create_usuarios_table.php
@@ -14,6 +14,8 @@ class CreateUsuariosTable extends Migration
     public function up()
     {   
         // creamos tabla
+        /*
+        //Migración con Schema
         Schema::create('usuarios', function (Blueprint $table) {
             $table->increments('id');
             $table->string('user', 255);
@@ -23,7 +25,9 @@ class CreateUsuariosTable extends Migration
             // nos permite crear los campos la ultima vez que modifique registro y la primera vez que se creó
             $table->timestamps();
         });
-       
+        */
+
+        // Migracion con SQL
         DB::statement("
             CREATE TABLE usuarios(
                 id int(255) auto_increment not null,
